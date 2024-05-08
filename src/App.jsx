8555f08df6f3e4538d2750783b6db0c961ab5f57
Marcin Slusarek <mrvrota@gmail.com>
2024-05-08 21:17:09 +0200

    Add app-ads.txt

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" component={Home} />
         <Route path="/tos" component={Tos} />
         <Route path="/pp" component={Pp} />
+        <Route path="/app-ads.txt" element={<div>google.com, pub-8202922806431205, DIRECT, f08c47fec0942fa0</div>} />
       </Routes>
     </>
   );
